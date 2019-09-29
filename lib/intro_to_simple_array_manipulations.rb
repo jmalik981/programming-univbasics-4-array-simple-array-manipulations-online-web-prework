9c301101e370255d6d533b454616b5bc14d0eaea
Junaid Malik <jmalik981@gmail.com>
2019-09-29 19:14:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,7 +45,9 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
 
-
+def using_delete_at(famous_robots, 2)
+  famous_robots.delete(2)
+end
   
   
   
