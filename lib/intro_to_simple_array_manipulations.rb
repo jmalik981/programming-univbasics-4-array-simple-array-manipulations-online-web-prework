0636b2f34c2f59e184f3ca52f4633dee84f4ff66
Junaid Malik <jmalik981@gmail.com>
2019-09-29 18:58:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,5 +30,6 @@ def using_concat(my_favorite_things, all_my_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  
+  list_of_programming_languages.insert(4, another_language)
+end
   
