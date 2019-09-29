a87d9406615749b15c0fbb4522be1bf8caa0b04d
Junaid Malik <jmalik981@gmail.com>
2019-09-29 18:43:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, element)
+
+  array.unshift(element)
+  end
+  
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(dog_breeds)
+dog_breeds.pop(2)
+end
+
+def using_shift()
+  
